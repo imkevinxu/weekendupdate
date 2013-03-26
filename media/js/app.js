a6e4526e1dd1b49705f7eaa0723e3599957c73ab
Kevin Xu <kevin@imkevinxu.com>
2013-03-26 15:10:17 -0700

    lot of refactoring

--- media/js/app.js
+++ media/js/app.js
@@ -48,8 +48,10 @@
     $("input[type=radio]").prop('checked', false);
   });
 
+  /* -----------------------------------------
+     ZURB FOUNDATION INITIALIZERS
+  ----------------------------------------- */
 
-  /* ZURB Foundation Javascript Initialization */
   var $doc = $(document);
   var Modernizr = window.Modernizr;
 
@@ -67,15 +69,6 @@
     $.fn.foundationClearing         ? $doc.foundationClearing() : null;
     $.fn.placeholder                ? $('input, textarea').placeholder() : null;
   });
-
-  $("#featured").orbit();
-  $('input, textarea').placeholder();
-
-  // UNCOMMENT THE LINE YOU WANT BELOW IF YOU WANT IE8 SUPPORT AND ARE USING .block-grids
-  // $('.block-grid.two-up>li:nth-child(2n+1)').css({clear: 'both'});
-  // $('.block-grid.three-up>li:nth-child(3n+1)').css({clear: 'both'});
-  // $('.block-grid.four-up>li:nth-child(4n+1)').css({clear: 'both'});
-  // $('.block-grid.five-up>li:nth-child(5n+1)').css({clear: 'both'});
 
   // Hide address bar on mobile devices (except if #hash present, so we don't mess up deep linking).
   if (Modernizr.touch) {
